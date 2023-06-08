2b29dbe8b786354dd681e0ecec11149dc3b3dcb3
Vladislav Samsonov <vl4d.s4m@gmail.com>
2023-06-09 00:02:51 +0300

    Add restrictions for input data

--- client/src/main/java/org/vl4ds4m/keyboardraces/client/LoginPaneController.java
+++ client/src/main/java/org/vl4ds4m/keyboardraces/client/LoginPaneController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,15 +17,33 @@ public class LoginPaneController {
     @FXML
     private TextField playerName;
     @FXML
+    private Text wrongName;
+    @FXML
     private TextField serverAddress;
     @FXML
     private TextField serverPort;
+    @FXML
+    private Text wrongPort;
 
     @FXML
     private void initialize() {
-        startButton.disableProperty().bind(serverAddress.textProperty().isEmpty());
-        startButton.disableProperty().bind(serverPort.textProperty().isEmpty());
-        startButton.disableProperty().bind(playerName.textProperty().isEmpty());
+        startButton.disableProperty().bind(serverAddress.textProperty().isEmpty()
+                .or(serverPort.textProperty().isEmpty())
+                .or(playerName.textProperty().isEmpty())
+                .or(wrongName.visibleProperty())
+                .or(wrongPort.visibleProperty()));
+
+        wrongName.visibleProperty().bind(playerName.textProperty().length().greaterThan(15));
+
+        serverPort.textProperty().addListener((observableValue, s, t1) -> {
+            int port;
+            try {
+                port = Integer.parseInt(t1);
+            } catch (NumberFormatException e) {
+                port = -1;
+            }
+            wrongPort.visibleProperty().set(port < 0 || port >= (1 << 16));
+        });
     }
 
     @FXML
@@ -58,7 +77,7 @@ public class LoginPaneController {
         info.setContentText("""
                 Игра 'Клавогонки' - соревновательная многопользовательская игра,
                 в которой Вы должны быстрее свои соперников набрать текст на клавиатуре.
-                
+                                
                 Автор реализации: Самсонов Владислав (vl4d.s4m@gmail.com).""");
 
         info.show();
